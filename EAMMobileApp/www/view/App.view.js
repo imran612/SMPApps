23108e83680416a4c15232fefa4167f9e4e849dd
imran612 <imran612@gmail.com>
2015-03-18 21:46:13 -0500

    eammobapp

--- EAMMobileApp/www/view/App.view.js
+++ EAMMobileApp/www/view/App.view.js
@@ -5,15 +5,21 @@ sap.ui.jsview("com.slb.mobile.view.App", {
 	},
 	
 	createContent: function (oController) {
-              alert("in app view createmethod");
         // to avoid scroll bars on desktop the root view must be set to block display
 		this.setDisplayBlock(true);
-        this.app = new sap.m.App();
+        this.app = new sap.m.SplitApp();
+              //this.app.setMode(sap.m.SplitAppMode.StretchCompressMode);
+        //this.app = new sap.m.App();
+              
+        this.app.addMasterPage(sap.ui.jsview("emptyview", "com.slb.mobile.view.Empty"));
         // load the login page
         var main = sap.ui.xmlview("mainview", "com.slb.mobile.view.mainview");
         main.getController().nav = this.getController();
-        this.app.addPage(main, true);
-         
-        return this.app;
+        this.app.addPage(main, false);
+              this.app.setMode(sap.m.SplitAppMode.HideMode);
+              //this.app.hideMaster();
+              
+       return this.app;
+            
 	}
 });
