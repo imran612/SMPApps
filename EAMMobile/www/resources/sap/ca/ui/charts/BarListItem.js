cc036ec0d6954f4e7e7454f883f83510ff1bdeb9
imran612 <imran@imran-mbp.gateway.pace.com>
2015-05-04 23:19:18 -0500

    delete eam

--- EAMMobile/www/resources/sap/ca/ui/charts/BarListItem.js
+++
@@ -1,7 +0,0 @@
-/*!
- * SAP UI development toolkit for HTML5 (SAPUI5) (c) Copyright 2009-2014 SAP SE. All rights reserved
- */
-jQuery.sap.declare("sap.ca.ui.charts.BarListItem");jQuery.sap.require("sap.ca.ui.library");jQuery.sap.require("sap.m.ListItemBase");sap.m.ListItemBase.extend("sap.ca.ui.charts.BarListItem",{metadata:{deprecated:true,library:"sap.ca.ui",properties:{"axis":{type:"string",group:"Misc",defaultValue:null},"group":{type:"string",group:"Misc",defaultValue:null},"value":{type:"string",group:"Data",defaultValue:null}}}});
-/*
- * Copyright (C) 2009-2014 SAP SE or an SAP affiliate company. All rights reserved
- */
--- EAMMobile/www/resources/sap/makit/CombinationChartRenderer.js
+++
@@ -1,7 +0,0 @@
-/*!
- * SAP UI development toolkit for HTML5 (SAPUI5)
- * 
- * (c) Copyright 2009-2014 SAP SE. All rights reserved
- */
-jQuery.sap.declare("sap.makit.CombinationChartRenderer");sap.makit.CombinationChartRenderer={};
-sap.makit.CombinationChartRenderer.render=function(r,c){r.write("<div id=\"sap-ui-dummy-"+c.getId()+"\" style=\"display:none\">");r.write("<div");r.writeControlData(c);r.writeAttribute("data-sap-ui-preserve",c.getId());r.addClass("sapMakitChart");r.writeClasses();r.write(">");r.write("</div>");r.write("</div>")};
